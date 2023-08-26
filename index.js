55b39ee514e92acc52a2d654f7ad39fd5cff0305
faith20233 <119449666+faith20233@users.noreply.github.com>
2023-08-26 17:49:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
 // ---
+const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont');
+const smallMenu = document.querySelector('.header__sm-menu');
+const headerHamMenuBtn = document.querySelector('.header__main-ham-menu');
+const headerHamMenuCloseBtn = document.querySelector('.header__main-ham-menu-close');
+const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link');
+
+hamMenuBtn.addEventListener('click', () => {
+  if (smallMenu.classList.contains('header__sm-menu--active')) {
+    smallMenu.classList.remove('header__sm-menu--active');
+  } else {
+    smallMenu.classList.add('header__sm-menu--active');
+  }
+  if (headerHamMenuBtn.classList.contains('d-none')) {
+    headerHamMenuBtn.classList.remove('d-none');
+    headerHamMenuCloseBtn.classList.add('d-none');
+  } else {
+    headerHamMenuBtn.classList.add('d-none');
+    headerHamMenuCloseBtn.classList.remove('d-none');
+  }
+});
+
+for (let i = 0; i < headerSmallMenuLinks.length; i++) {
+  headerSmallMenuLinks[i].addEventListener('click', () => {
+    smallMenu.classList.remove('header__sm-menu--active');
+    headerHamMenuBtn.classList.remove('d-none');
+    headerHamMenuCloseBtn.classList.add('d-none');
+  });
+}
+
+const headerLogoConatiner = document.querySelector('.header__logo-container');
+
+headerLogoConatiner.addEventListener('click', () => {
+  location.href = 'index.html';
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const sliderContainer = document.querySelector('.slider-container');
+  const slides = document.querySelectorAll('.slide');
+  const slideCount = slides.length;
+  let currentIndex = 0;
+
+  function nextSlide() {
+    currentIndex++;
+    if (currentIndex === slideCount) {
+      currentIndex = 0;
+    }
+    updateSlider();
+  }
+
+  function updateSlider() {
+    sliderContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+  }
+
+  // Automatically slide every 3 seconds
+  setInterval(nextSlide, 3000);
+});
 const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
 const smallMenu = document.querySelector('.header__sm-menu')
 const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
@@ -35,4 +91,24 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
-})
+document.addEventListener('DOMContentLoaded', function() {
+  const sliderContainer = document.querySelector('.slider-container');
+  const slides = document.querySelectorAll('.slide');
+  const slideCount = slides.length;
+  let currentIndex = 0;
+
+  function nextSlide() {
+    currentIndex++;
+    if (currentIndex === slideCount) {
+      currentIndex = 0;
+    }
+    updateSlider();
+  }
+
+  function updateSlider() {
+    sliderContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+  }
+
+  // Automatically slide every 3 seconds
+  setInterval(nextSlide, 3000);
+});
